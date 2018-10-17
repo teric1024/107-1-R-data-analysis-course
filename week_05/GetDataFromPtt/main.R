1e1bd707f7fee808e5c001de45af170f3a98ec4d
user <erictu1219@gmail.com>
2018-10-17 23:32:35 +0800

    update hw5

--- week_05/GetDataFromPtt/main.R
+++
@@ -1,8 +0,0 @@
-source('pttTestFunction.R')
-id = c(17412:17550)
-#from 10/10 to 10/17
-URL = paste0("https://www.ptt.cc/bbs/C_Chat/index", id, ".html")
-filename = paste0(id, ".txt")
-pttTestFunction(URL[1], filename[1])
-mapply(pttTestFunction, 
-       URL = URL, filename = filename)
