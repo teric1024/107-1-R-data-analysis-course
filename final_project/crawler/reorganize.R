c42733146474580bb58499b2edb1c92a8a3fb90b
user <erictu1219@gmail.com>
2018-12-15 23:22:28 +0800

    add sorted data

---
+++ final_project/crawler/reorganize.R
@@ -0,0 +1,26 @@
+rm(list=ls(all.names = TRUE))
+
+filenames <- list.files(getwd(), pattern="*.csv")
+
+sortdata <- function(filename){
+  data1<-read.table(filename, header=T, sep="\t")
+  areaname <- as.vector(data1[["地區"]])
+  split.area <- function(name){
+    name <- as.character(name)
+    return (c(strsplit(name,split=" ",fixed=T)))
+  }
+  areaname <- as.data.frame(sapply(areaname, split.area))
+  areaname <- t(areaname)
+  data1$"縣市" <- areaname[,1]
+  data1$"鄉鎮市區" <- areaname[,2]
+  data1$"X" <- NULL
+  data1$"地區" <- NULL
+  return (data1)
+}
+
+alldata <- data.frame()
+for (x in filenames){
+  data.vote <- sortdata(x)
+  alldata <- rbind(data.vote, alldata)
+}
+write.table(alldata, file = "all_votes_data.csv", sep = ",")
